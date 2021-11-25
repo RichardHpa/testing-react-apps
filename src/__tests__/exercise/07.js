0de205e056afebd9274b078564d199bf463e6c49
RichardHpa <richard.m.hpa@gmail.com>
2021-11-26 11:52:15 +1300

    created wrapper for themeProvider

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -6,12 +6,22 @@ import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
-test('renders with the light styles for the light theme', () => {
+function renderWithTheme(ui, {theme = 'light', ...options} = {}) {
   const Wrapper = ({children}) => (
-    <ThemeProvider initialTheme="light">{children}</ThemeProvider>
+    <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
   )
 
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  return render(ui, {wrapper: Wrapper, ...options})
+}
+
+test('renders with the light styles for the light theme', () => {
+  // const Wrapper = ({children}) => (
+  //   <ThemeProvider initialTheme="light">{children}</ThemeProvider>
+  // )
+
+  // render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  // renderWithTheme(<EasyButton>Easy</EasyButton>, 'light')
+  renderWithTheme(<EasyButton>Easy</EasyButton>)
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
@@ -20,11 +30,13 @@ test('renders with the light styles for the light theme', () => {
 })
 
 test('renders with dark styles for the dark theme', () => {
-  const Wrapper = ({children}) => (
-    <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
-  )
+  // const Wrapper = ({children}) => (
+  //   <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  // )
 
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  // render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  // renderWithTheme(<EasyButton>Easy</EasyButton>, 'dark')
+  renderWithTheme(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: black;
