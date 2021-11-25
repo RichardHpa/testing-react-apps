b2577d48ec9dfbf0d5ee392a24acc93c0f891c18
RichardHpa <richard.m.hpa@gmail.com>
2021-11-26 10:08:02 +1300

    completed exercise 4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,23 +5,42 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
+
+const buildLoginForm = build({
+  fields: {
+    username: fake(faker => faker.internet.userName()),
+    password: fake(faker => faker.internet.password()),
+  },
+})
+
+// const buildLoginForm = overrides => {
+//   return {
+//     username: faker.internet.userName(),
+//     password: faker.internet.password(),
+//     ...overrides,
+//   }
+// }
 
 test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
+  const {username, password} = buildLoginForm()
+  // let submittedData = {}
   // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use userEvent.type to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  const handleSubmit = jest.fn()
+  render(<Login onSubmit={handleSubmit} />)
+  // const username = 'Jimmy'
+  // const password = '12345'
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.type(screen.getByLabelText(/password/i), password)
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  // expect(submittedData).toEqual({
+  //   username,
+  //   password,
+  // })
+  expect(handleSubmit).toHaveBeenCalledWith({username, password})
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
 
 /*
